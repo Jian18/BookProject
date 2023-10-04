c05e47310f815a6a161e942aa4f0df105091cedb
Jian18 <jian.kootker@gmail.com>
2023-10-04 10:04:01 +0200

    classes toegevoegd

---
+++ Entities/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace BookProject.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookProject.Models
+{
+    public class Book
+    {
+        public long  ISBN { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+
+    }
+}
